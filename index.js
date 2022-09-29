0a1c7cf14c42197e2fc9372780b2208bbde2bbb6
colefrazier <cfrazier541@gmail.com>
2022-09-29 10:47:51 -0400

    commit tree ccf271fb1fc5dece4151d72cb144154cce455083

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+console.log("left", left)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  console.log("right", left)
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+    console.log(e.key)
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+   else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
